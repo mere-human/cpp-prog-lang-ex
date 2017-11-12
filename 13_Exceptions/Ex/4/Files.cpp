6d87ff717357578e04b136463d203400d414a733
Gridelen <zaychenko.gennadiy@gmail.com>
2017-11-12 13:30:07 +0200

    Implement read & write functions for ex.13-4
    Improve error information for open & close functions.
    Add .tmp files to ignore list (it's produced by this ex.).

--- 13_Exceptions/Ex/4/Files.cpp
+++ 13_Exceptions/Ex/4/Files.cpp
@@ -4,41 +4,97 @@ from the C interface to your operating system and provide equivalent C++
 functions that call the C functions but throw exceptions in case of errors.
 */
 
+#ifdef _MSC_VER
+// define this to avoid MSVC error: fopen may be unsafe
+#define _CRT_SECURE_NO_WARNINGS 1
+#endif
+
 #include <cstdio>
 #include <exception>
 #include <iostream>
+#include <sstream>
 
-struct IOException : public std::exception
+struct IOException
 {
-    IOException(const char* const message) : std::exception{message}
-    {}
+	const std::string message;
 };
 
 FILE* open_file(const char* file_name, const char* mode)
 {
     auto fp = fopen(file_name, mode);
-    if (!fp) throw IOException{"failed to open file"};
+	if (!fp)
+	{
+		std::ostringstream buffer;
+		buffer << "failed to open file '" << file_name
+                        << "' with mode '" << mode << "'";
+		throw IOException{ buffer.str() };
+	}
     return fp;
 }
 
 void close_file(FILE* fp)
 {
-    if (ferror(fp)) throw IOException{"file contains errors"};
-    if (fclose(fp) != 0) throw IOException{"failed to close file"};
+	if (fclose(fp) != 0)
+		throw IOException{ "error while closing a file" };
+}
+
+char read_char(FILE* fp)
+{
+	int c = fgetc(fp);
+	if (c == EOF)
+	{
+		if (feof(fp))
+			throw IOException{ "end of file reached while reading" };
+		else
+			throw IOException{ "failed to read" };
+	}
+	if ((c > CHAR_MAX) || (c < CHAR_MIN))
+	{
+		std::ostringstream buffer;
+		buffer << c << " character is out of range";
+		throw IOException{ buffer.str() };
+	}
+	return static_cast<char>(c);
+}
+
+void write_char(FILE* fp, char ch)
+{
+	auto res = fputc(ch, fp);
+	if (res != ch)
+	{
+		if (res == EOF)
+			throw IOException{ "failed to write" };
+		else
+			throw IOException{ "unkown error while writing" };
+	}
 }
 
 int main()
 {
+	// NOTE: it is better to use RAII solution like File_ptr
     try
     {
-        auto fp = open_file("foo", "r");
-        close_file(fp);
+        auto fp = open_file("a.tmp", "r");
+        close_file(fp); // unreachable
     }
     catch (IOException& e)
     {
-        std::cerr << "Error: " << e.what() << "\n";
-        return 1;
+        std::cerr << "I/O Error: " << e.message << "\n";
     }
+
+	FILE* fp = nullptr;
+	try
+	{
+		fp = open_file("a.tmp", "w+");
+		auto c = read_char(fp);
+		std::cout << "Read: " << c << "\n"; // unreachable
+	}
+	catch (IOException& e)
+	{
+		std::cerr << "I/O Error: " << e.message << "\n";
+	}
+	if (fp)
+		close_file(fp);
     return 0;
 }
 
