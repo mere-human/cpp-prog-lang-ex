06519ec269c60d98041b9b720a8792bc707611c5
Gridelen <zaychenko.gennadiy@gmail.com>
2017-11-30 00:02:13 +0200

    Explicitly qualify definition of Token_stream::get (ex.14-2)

--- 14_Namespaces/Ex/2/Calc.cpp
+++ 14_Namespaces/Ex/2/Calc.cpp
@@ -85,45 +85,45 @@ namespace Lexer {
 
 	Token_stream ts{ cin }; // use input from cin
 
-	Token Token_stream::get()
-	{
-		char ch;
-		do { // skip whitespace except '\n'
-			if (!ip->get(ch)) return ct = { Kind::end };
-		} while (ch != '\n' && isspace(ch));
-
-		switch (ch) {
-		case ';':
-		case '\n':
-			return ct = { Kind::print };
-		case '*':
-		case '/':
-		case '+':
-		case '-':
-		case '(':
-		case ')':
-		case '=':
-			return ct = { static_cast<Kind>(ch) };
-		case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
-		case '.':
-			ip->putback(ch);        // put the first digit (or .) back into the input stream
-			*ip >> ct.number_value; // read the number into ct
-			ct.kind = Kind::number;
-			return ct;
-		default:  // name, name =, or error
-			if (isalpha(ch)) {
-				ct.string_value = ch;
-				while (ip->get(ch) && isalnum(ch))
-					ct.string_value += ch; // append ch to end of string_value
-				ip->putback(ch);
-				ct.kind = Kind::name;
-				return ct;
-			}
-			throw Error::SyntaxError{ "bad token" };
-		}
-	}
-
 } // namespace Lexer
+
+Lexer::Token Lexer::Token_stream::get()
+{
+	char ch;
+	do { // skip whitespace except '\n'
+		if (!ip->get(ch)) return ct = { Kind::end };
+	} while (ch != '\n' && isspace(ch));
+
+	switch (ch) {
+	case ';':
+	case '\n':
+		return ct = { Kind::print };
+	case '*':
+	case '/':
+	case '+':
+	case '-':
+	case '(':
+	case ')':
+	case '=':
+		return ct = { static_cast<Kind>(ch) };
+	case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+	case '.':
+		ip->putback(ch);        // put the first digit (or .) back into the input stream
+		*ip >> ct.number_value; // read the number into ct
+		ct.kind = Kind::number;
+		return ct;
+	default:  // name, name =, or error
+		if (isalpha(ch)) {
+			ct.string_value = ch;
+			while (ip->get(ch) && isalnum(ch))
+				ct.string_value += ch; // append ch to end of string_value
+			ip->putback(ch);
+			ct.kind = Kind::name;
+			return ct;
+		}
+		throw Error::SyntaxError{ "bad token" };
+	}
+}
 
 namespace Table {
 	map<string, double> table;
