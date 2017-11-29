46e296d5a9a49cd1f4dffb549c2d2a0b8a0ab1ce
Gridelen <zaychenko.gennadiy@gmail.com>
2017-11-29 23:59:55 +0200

    Add separate implementor interface for Parser (ex.14-2)

--- 14_Namespaces/Ex/2/Calc.cpp
+++ 14_Namespaces/Ex/2/Calc.cpp
@@ -129,89 +129,93 @@ namespace Table {
 	map<string, double> table;
 } // namespace Table
 
-namespace Parser {
-
-	double prim(bool get);
-	double term(bool get);
+namespace Parser { // user interface
 	double expr(bool);
-
 } // namespace Parser
 
-namespace Parser {
+namespace Parser_impl { // implementer interface
 
-	using Lexer::ts;
+	using namespace Parser;
+
+	double prim(bool);
+	double term(bool);
+
+	using namespace Lexer; // use all facilities offered by Lexer
 	using Lexer::Kind;
 	using Error::SyntaxError;
 
-	double prim(bool get)
+} // namespace Parser_impl
+
+double Parser_impl::prim(bool get)
+{
+	if (get) ts.get();
+	switch (ts.current().kind) {
+	case Kind::number: // floating-point constant
 	{
-		if (get) ts.get();
+		double v = ts.current().number_value;
+		ts.get();
+		return v;
+	}
+	case Kind::name:
+	{
+		double& v = Table::table[ts.current().string_value]; // find the corresponding
+		if (ts.get().kind == Kind::assign) v = expr(true); // '=' seen: assignment
+		return v;
+	}
+	case Kind::minus:
+		return -prim(true);
+	case Kind::lp:
+	{
+		auto e = expr(true);
+		if (ts.current().kind != Kind::rp) throw SyntaxError{ "')' expected" };
+		ts.get(); // eat ')'
+		return e;
+	}
+	default:
+		throw SyntaxError{ "primary expected" };
+	}
+}
+
+double Parser_impl::term(bool get)
+{
+	double left = prim(get);
+	for (;;) {
 		switch (ts.current().kind) {
-		case Kind::number: // floating-point constant
-		{
-			double v = ts.current().number_value;
-			ts.get();
-			return v;
-		}
-		case Kind::name:
-		{
-			double& v = Table::table[ts.current().string_value]; // find the corresponding
-			if (ts.get().kind == Kind::assign) v = expr(true); // '=' seen: assignment
-			return v;
-		}
-		case Kind::minus:
-			return -prim(true);
-		case Kind::lp:
-		{
-			auto e = expr(true);
-			if (ts.current().kind != Kind::rp) throw SyntaxError{ "')' expected" };
-			ts.get(); // eat ')'
-			return e;
-		}
+		case Kind::mul:
+			left *= prim(true);
+			break;
+		case Kind::div:
+			if (auto d = prim(true)) {
+				left /= d;
+				break;
+			}
+			throw SyntaxError{ "divide by 0" };
 		default:
-			throw SyntaxError{ "primary expected" };
+			return left;
 		}
 	}
+}
 
-	double term(bool get)
-	{
-		double left = prim(get);
-		for (;;) {
-			switch (ts.current().kind) {
-			case Kind::mul:
-				left *= prim(true);
-				break;
-			case Kind::div:
-				if (auto d = prim(true)) {
-					left /= d;
-					break;
-				}
-				throw SyntaxError{ "divide by 0" };
-			default:
-				return left;
-			}
+double Parser::expr(bool get)
+{
+	using namespace Parser_impl;
+
+	double left = term(get);
+	for (;;) { // forever
+		switch (ts.current().kind) {
+		case Kind::plus:
+			left += term(true);
+			break;
+		case Kind::minus:
+			left -= term(true);
+			break;
+		default:
+			return left;
 		}
 	}
+	return left;
+}
 
-	double expr(bool get)
-	{
-		double left = term(get);
-		for (;;) { // forever
-			switch (ts.current().kind) {
-			case Kind::plus:
-				left += term(true);
-				break;
-			case Kind::minus:
-				left -= term(true);
-				break;
-			default:
-				return left;
-			}
-		}
-		return left;
-	}
-
-} // namespace Parser
 
 namespace Driver {
 
